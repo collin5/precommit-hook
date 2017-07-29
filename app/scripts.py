075eef836e29238f3de49893b2b5a7d9e46468e9
Collins Abitekaniza <abtcolns@gmail.com>
2017-07-29 15:11:01 +0300

    Add post egg_info and post develop scripts

--- app/scripts.py
+++ app/scripts.py
@@ -1,33 +1,68 @@
-#@Author: Collins Abitekanza
-#@Email: abtcolns@gmail.com
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# app/scripts.py
+# Copyright (c) 2017 Collins Abitekaniza <abtcolns@gmail.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
-from distutils.command.install import install
+from setuptools.command.install import install
+from setuptools.command.develop import develop
+from setuptools.command.egg_info import egg_info
 import os
 
-class Post_install(install):
-    
+
+class Exec:
     @staticmethod
     def add_pre_commit():
 
-        #if .git directory exists
+        # if .git directory exists
         if os.path.isdir(os.path.join(os.getcwd(), ".git")):
-                #make hook directory if not exists
-                if not os.path.isdir(os.path.join(os.getcwd(), ".git/hooks")):
-                    os.system("mkdir .git/hooks")
+            # make hook directory if not exists
+            if not os.path.isdir(os.path.join(os.getcwd(), ".git/hooks")):
+                os.system("mkdir -p .git/hooks")
 
-                #finally copy and set permissions
-                os.system("cp app/tmp/template.dat .git/hooks/pre-commit && sudo chmod +x .git/hooks/pre-commit")
+            # finally copy and set permissions
+            os.system(
+                "cp app/tmp/template.dat .git/hooks/pre-commit && sudo chmod +x .git/hooks/pre-commit")
+            print("Precommit added successfully, continuing ...")
         else:
-            print("Error installing precommit-hook : .git directory not found in current working path")
-            return False
+            print(
+                "Error installing precommit-hook : .git directory not found in current working path")
         return True
+
+
+class Post_install(install):
 
     def run(self):
         install.run(self)
-        #Pre install actions
-        if Post_install.add_pre_commit():
-            print("Bingo: Pre-commit successfully installed, bye bye ....")
-        else:
-            print("Failed to add hook,, continuing ...")
-            
+        Exec.add_pre_commit()
 
+
+class Post_develop(develop):
+
+    def run(self):
+        develop.run(self)
+        Exec.add_pre_commit()
+
+
+class Post_egg_info(egg_info):
+
+    def run(self):
+        egg_info.run(self)
+        Exec.add_pre_commit()
--- setup.py
+++ setup.py
@@ -1,25 +1,48 @@
-#@Author: Collins Abitekaniza
-#@Email: abtcolns@gmail.com
-
-
-from distutils import setup
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# setup.py
+# Copyright (c) 2017 Collins Abitekaniza <abtcolns@gmail.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 from app.scripts import *
+from setuptools import setup
+from setuptools.command.install import install
+from setuptools.command.develop import develop
+from setuptools.command.egg_info import egg_info
 
 setup(
 
-        #Application name
-        name = "precommit-hook",
-        
-        version = "0.1",
-        author = "Collins Abitekaniza",
-        author_email = "abtcolns@gmail.com",
-        packages = ['app'],
-        include_package_data = True,
-        license = 'MIT',
-        url = "https://github.com/collin5/precommit-hook",
-        description = "Enforce code quality in python projects",
-        install_requires = ["flake8"],
-        cmdclass = {
-            "install":Post_install
-        }
-    )
+    # Application name
+    name="precommit-hook",
+
+    version="0.0.1",
+    author="Collins Abitekaniza",
+    author_email="abtcolns@gmail.com",
+    packages=['app'],
+    include_package_data=True,
+    license='MIT',
+    url="https://github.com/collin5/precommit-hook",
+    description="Enforce code quality in python projects",
+    install_requires=["flake8"],
+    cmdclass={
+            "install": Post_install,
+            "develop": Post_develop,
+            "egg_info": Post_egg_info
+    }
+)

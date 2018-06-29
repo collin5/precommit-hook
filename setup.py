68c6105465bf5af9503941e2db6d5eeca9c70c77
Marco Lovato <maglovato@gmail.com>
2018-06-29 11:56:57 -0300

    fix to meet codacy standards

--- setup.py
+++ setup.py
@@ -3,19 +3,19 @@
 
 from app.scripts import Post_install
 from setuptools import setup, find_packages
-from codecs import open
+import codecs
 from os import path
 
 here = path.abspath(path.dirname(__file__))
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with codecs.open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 # Get the requirements from the requirements.txt file
-with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+with codecs.open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
     requirements = f.read()
 
 # Get the dev requirements from the requirements-dev.txt file
-with open(path.join(here, 'requirements-dev.txt'), encoding='utf-8') as f:
+with codecs.open(path.join(here, 'requirements-dev.txt'), encoding='utf-8') as f:
     requirements_dev = f.read()
 
 setup(

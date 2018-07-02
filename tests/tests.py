d89d3d9da356d49472792cbf34d27d344f382936
Marco Lovato <maglovato@gmail.com>
2018-07-02 13:10:31 -0300

    ignoring warnings since are only test scripts

--- tests/tests.py
+++ tests/tests.py
@@ -9,8 +9,8 @@ class BaseTestCase(TestCase):
 
     def setUp(self):
         # create test temporary directory
-        os.system("mkdir -p /tmp/precommit-hook-tests && cd /tmp/precommit-hook-tests && git init")
+        os.system("mkdir -p /tmp/precommit-hook-tests && cd /tmp/precommit-hook-tests && git init")  # noqa
 
     def tearDown(self):
         # remove all test files
-        os.system("rm -r /tmp/precommit-hook-tests")
+        os.system("rm -r /tmp/precommit-hook-tests")  # noqa
